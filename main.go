bfd07d579901d59bb3a0302bb11c7b9f15058864
Amos Pöttner <amos@MacBook-Air-von-Amos.local>
2022-06-18 00:10:29 +0200

    add file input and comment to main method

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
-import ("fmt")
+
+import (
+	"fmt"
+)
+
+/*
+Amos Pöttner, amospoettner@outlook.de, unknowncowman@github.com
+
+This programm is supposed to open a file on a server and edit it via a web interface
+*/
 
 func main() {
-  fmt.Println("Gutendabend Welt!")
+	fmt.Println("Gutendabend Welt!")
 }
